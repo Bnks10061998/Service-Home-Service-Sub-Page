c273f3d28b8129333625061b54af0b284854dc9c
LeastAction25 <leastactioncompany@gmail.com>
2025-05-20 07:30:07 +0530

    dheena

--- src/Components/NotFoundPage.jsx
+++ src/Components/NotFoundPage.jsx
@@ -14,7 +14,7 @@ const NotFoundPage = () => {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 bg-light text-center not-found-container">
       <div>
-        <h1 className="display-1 text-danger fw-bold">404</h1>
+        <h1 className="text-[6rem] text-red-600 font-extrabold">404</h1>
         <h2 className="fw-semibold">Oops! Page Not Found</h2>
         <p className="custom-404-text">
           It seems like you've wandered off the path. The page you're
@@ -25,7 +25,7 @@ const NotFoundPage = () => {
         </p>
           <div className="flex items-center justify-center ">
         <button
-          className=" btn btn-primary flex items-center justify-center  px-8 text-lg space-x-4"
+          className="flex items-center justify-center px-8 py-2 text-lg text-white bg-[#003a84] hover:bg-[#002f6b] rounded-xl space-x-2"
           onClick={handleBackToHome}
         >
           <FaHome className="me-5 " />
